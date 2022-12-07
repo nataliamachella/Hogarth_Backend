ea99030374bba5b78d69789fbc0c93fcffe6eeac
lTenryuu <cuau_daali@hotmail.com>
2022-12-07 00:46:10 -0600

    fixed

--- controllers/notesControllers.js
+++ controllers/notesControllers.js
@@ -1,33 +1,54 @@
 const notesServices = require("../services/notesServices.js");
 
 // GET ALL NOTES
-exports.index = (req, res) => {
+exports.index = (req, res, next) => {
   notesServices
     .findAll()
     .then((notes) => res.status(200).send(notes))
-    .catch((err) => res.status(400).send(err));
+    .catch(next);
+};
+
+exports.findForPagination = (req, res, next) => {
+  const { page } = req.params;
+  notesServices
+    .findAllPagination(page)
+    .then((data) => {
+      const { count: totalItems, rows: notes } = data;
+      const currentPage = page ? +page : 0;
+      const totalPages = Math.ceil(totalItems / 12) - 1;
+      res.status(200).send({ totalItems, notes, totalPages, currentPage });
+    })
+    .catch(next);
+};
+
+exports.findForLimit = (req, res, next) => {
+  const { limit } = req.params;
+  notesServices
+    .findAllWithLimit(limit)
+    .then((notes) => res.status(200).send(notes))
+    .catch(next);
 };
 
 // GET NOTE BY URL
-exports.findByUrl = (req, res) => {
+exports.findByUrl = (req, res, next) => {
   const { url } = req.params;
   notesServices
     .findByUrl(url)
     .then((note) => res.status(200).send(note))
-    .catch((err) => res.status(400).send(err));
+    .catch(next);
 };
 
 // SEARCH NOTE
-exports.searchByQueryString = (req, res) => {
+exports.searchByQueryString = (req, res, next) => {
   const { queryString } = req.query;
   notesServices
     .searchByQueryString(queryString)
     .then((notes) => res.status(200).send(notes))
-    .catch((err) => res.status(400).send(err));
+    .catch(next);
 };
 
 //Find by category url
-exports.findByCategory = (req, res) => {
+exports.findByCategory = (req, res, next) => {
   const { url } = req.params;
   notesServices
     .findByCategory(url)
@@ -35,27 +56,27 @@ exports.findByCategory = (req, res) => {
       let notesFilter = notes.filter((note) => note.subCategory !== null);
       res.status(200).send(notesFilter);
     })
-    .catch((err) => res.status(400).send(err));
+    .catch(next);
 };
 
 //ADMIN CONTROLLERS
 
 // CREATE NEW NOTE
-exports.createNote = (req, res) => {
+exports.createNote = (req, res, next) => {
   const note = req.body;
   notesServices
     .create(note)
     .then((createdNote) => res.status(201).send(createdNote))
-    .catch((err) => res.status(400).send(err));
+    .catch(next);
 };
 
 // CHANGE NOTE
-exports.changeNote = (req, res) => {
+exports.changeNote = (req, res, next) => {
   const { id } = req.params;
   notesServices
     .change(id, req.body)
     .then((updatedNote) => res.status(200).send(updatedNote))
-    .catch((err) => res.status(404).send(err));
+    .catch(next);
 };
 
 exports.deleteContent = (req, res, next) => {
@@ -67,10 +88,10 @@ exports.deleteContent = (req, res, next) => {
 };
 
 // DELETE NOTE
-exports.deleteNote = (req, res) => {
+exports.deleteNote = (req, res, next) => {
   const { id } = req.params;
   notesServices
     .delete(id)
     .then(() => res.status(204).send("Note deleted"))
-    .catch((err) => res.status(404).send(err));
+    .catch(next);
 };
